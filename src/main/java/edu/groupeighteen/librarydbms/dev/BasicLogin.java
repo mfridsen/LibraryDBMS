409fae88e9c70e8ce6af710c751284b4aa2667d6
mfridsen <120990597+mfridsen@users.noreply.github.com>
2023-04-17 16:44:06 +0200

    Added dev package with two basic classes

---
+++ src/main/java/edu/groupeighteen/librarydbms/dev/BasicLogin.java
@@ -0,0 +1,23 @@
+package edu.groupeighteen.librarydbms.dev;
+
+/**
+ * @author Mattias Fridsén
+ * @project LibraryDBMS
+ * @package edu.groupeighteen.librarydbms.dev
+ * @contact matfir-1@student.ltu.se
+ * @date 4/17/2023
+ * <p>
+ * We plan as much as we can (based on the knowledge available),
+ * When we can (based on the time and resources available),
+ * But not before.
+ */
+public class BasicLogin {
+
+    public BasicLogin() {
+
+    }
+
+    /*********************************** Getters and Setters are self-explanatory. ************************************/
+
+
+}
