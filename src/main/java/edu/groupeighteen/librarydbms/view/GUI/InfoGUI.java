e6be6f17c2fd229682d4c6b836187ba4752fa7b8
jespertruedsson <jeppe.truedsson@gmail.com>
2023-04-21 14:30:51 +0200

    jg

--- src/main/java/edu/groupeighteen/librarydbms/view/GUI/InfoGUI.java
+++ src/main/java/edu/groupeighteen/librarydbms/view/GUI/InfoGUI.java
@@ -1,9 +1,45 @@
 package edu.groupeighteen.librarydbms.view.GUI;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * @author Johan Lund
  * @project LibraryDBMS
  * @date 2023-04-21
  */
 public class InfoGUI {
+    public JFrame firstFrame;
+    public JPanel firstPanel;
+    public JTextField firstInfoField;
+    public JTextField lastInfoField;
+    public JButton tillbakaButton;
+
+    public void showInfoGUI(){
+        // skapar infoGUI
+        firstFrame = new JFrame("InfoGUI");
+        firstPanel = new JPanel();
+        firstInfoField = new JTextField(20);
+        lastInfoField = new JTextField(20);
+        tillbakaButton = new JButton("Tillbaka");
+
+        firstPanel.add(new JLabel("Hej och välkommen till Lilla Biblioteket"));
+        firstPanel.add(firstInfoField);
+
+        firstPanel.add(new JLabel("Våra öppetider är: "));
+        firstPanel.add(lastInfoField);
+
+        firstPanel.add(tillbakaButton);
+
+        tillbakaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+
+
+    }
 }
