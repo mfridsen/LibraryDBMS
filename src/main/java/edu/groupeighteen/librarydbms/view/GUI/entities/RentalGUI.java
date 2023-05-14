18ba91fd167d0b3728087ad8347d52f1f88cf6db
mfridsen <120990597+mfridsen@users.noreply.github.com>
2023-05-14 16:12:02 +0200

    labels and buttons

--- src/main/java/edu/groupeighteen/librarydbms/view/GUI/entities/RentalGUI.java
+++ src/main/java/edu/groupeighteen/librarydbms/view/GUI/entities/RentalGUI.java
@@ -1,5 +1,9 @@
 package edu.groupeighteen.librarydbms.view.GUI.entities;
 
+import edu.groupeighteen.librarydbms.model.entities.Rental;
+
+import javax.swing.*;
+
 /**
  * @author Mattias Fridsén
  * @project LibraryDBMS
@@ -14,12 +18,34 @@ package edu.groupeighteen.librarydbms.view.GUI.entities;
  * Brought to you by enough nicotine to kill a large horse.
  */
 public class RentalGUI {
+    private final Rental rental;
 
-    public RentalGUI() {
+    private JLabel rentalIDLabel;
+    private JLabel userIDLabel;
+    private JLabel usernameLabel;
+    private JLabel itemIDLabel;
+    private JLabel itemTitleLabel;
+    private JLabel rentalDateLabel;
 
+    private JButton backButton;
+    private JButton updateButton;
+    private JButton deleteButton;
+
+    private JPanel RentalGUIPanel;
+    private JFrame RentalGUIFrame;
+
+    public RentalGUI(Rental rental) {
+        this.rental = rental;
+        this.rentalIDLabel  = new JLabel("Rental ID:    " + rental.getRentalID());
+        this.userIDLabel    = new JLabel("User ID:      " + rental.getUserID());
+        this.usernameLabel  = new JLabel("Username:     " + rental.getUsername());
+        this.itemIDLabel    = new JLabel("Item ID:      " + rental.getItemID());
+        this.itemTitleLabel = new JLabel("Item Title:   " + rental.getTitle());
+        this.rentalDateLabel = new JLabel("Rental Date:  " + rental.getRentalDate());
     }
 
     /*********************************** Getters and Setters are self-explanatory. ************************************/
-
-
+    public Rental getRental() {
+        return rental;
+    }
 }
