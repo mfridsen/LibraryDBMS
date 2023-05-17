1e621930aae7ee9e58763c8a7f9cac65122c449a
jespertruedsson <jeppe.truedsson@gmail.com>
2023-05-17 18:34:12 +0200

    hyhhy

--- src/main/java/edu/groupeighteen/librarydbms/view/GUI/entities/user/UserUpdateGUI.java
+++ src/main/java/edu/groupeighteen/librarydbms/view/GUI/entities/user/UserUpdateGUI.java
@@ -18,18 +18,36 @@ import java.util.Arrays;
  * @date 2023-04-24
  */
 public class UserUpdateGUI extends GUI {
-    private JTextField usernameField;
-    private JPasswordField passwordField;
+    private final User oldUser;
+    private User newUser;
     private User user;
+
+    private JTextField usernameField;
+    private JTable userUpdateTable;
+    private JPasswordField passwordField;
     private JPanel scrollPanePanel;
 
 
-    public UserUpdateGUI(GUI previousGUI, User user) {
-        super(previousGUI, "UserUpdateGUI");
-        this.user = user;
+    public UserUpdateGUI(GUI previousGUI, User userToUpdate) {
+        super(previousGUI, "UserUpdateGUI" + userToUpdate.getUserID());
+        this.oldUser = userToUpdate;
         setupScrollPane();
         setupPanels();
         displayGUI();
+    }
+
+    private void setupScrollPane() {
+        String[] columNames = {"Old Name", "New Name"};
+        Object[][] data = {
+                {"User ID", oldUser.getUserID(), ""},
+                {"Username", oldUser.getUsername(), ""},
+        };
+        userUpdateTable = setupTableWithEditableCells(columnNames, data, 2);
+
+        JScrollPane userScrollPane = new JScrollPane();
+        userScrollPane.setViewportView(userUpdateTable);
+
+
     }
 
     protected JButton[] setupButtons() {
@@ -41,7 +59,7 @@ public class UserUpdateGUI extends GUI {
             LibraryManager.getCurrentUser().setPassword(Arrays.toString(passwordField.getPassword()));
             try {
                 UserHandler.updateUser(LibraryManager.getCurrentUser());
-                new MenuPageGUI(LibraryManager.getCurrentUser(),this);
+                new MenuPageGUI(LibraryManager.getCurrentUser(), this);
 
             } catch (SQLException ex) {
                 throw new RuntimeException(ex);//TODO-Exception
@@ -50,35 +68,32 @@ public class UserUpdateGUI extends GUI {
         });
         return new JButton[]{changeButton};
 
-    private void setupScrollPane() {
-        String[] columNames = {"Old Name", "New Name"};
-        Object[][] data = {
-                {"User ID", oldUser.getUserID(), ""},
-                {"Username", oldRental.getUsername(), ""},
-                {"Item ID", oldRental.getItemID(), ""},
-                {"Item Title", oldRental.getTitle(), ""},
-        };
     }
 
+    private void resetCells() {
+        for (int row = 0; row < userUpdateTable.getRowCount(); row++) {
+            for (int col = 0; col < userUpdateTable.getColumnCount(); col++) {
+                if (col == 2) { //Assuming the 3rd column is the editable column
+                    userUpdateTable.setValueAt("", row, col);
+                }
 
-    @Override
+            }
 
-    }
 
-    @Override
-    protected void setupPanels() {
-        JLabel usernameLabel = new JLabel("Username:");
-        usernameField = new JTextField(10);
-        JLabel passwordLabel = new JLabel("Password:");
-        passwordField = new JPasswordField(10);
-        JPanel accountPanel = new JPanel();
+            protected void setupPanels () {
+                JLabel usernameLabel = new JLabel("Username:");
+                usernameField = new JTextField(10);
+                JLabel passwordLabel = new JLabel("Password:");
+                passwordField = new JPasswordField(10);
+                JPanel accountPanel = new JPanel();
 
-        accountPanel.add(usernameLabel);
-        accountPanel.add(usernameField);
-        accountPanel.add(passwordLabel);
-        accountPanel.add(passwordField);
+                accountPanel.add(usernameLabel);
+                accountPanel.add(usernameField);
+                accountPanel.add(passwordLabel);
+                accountPanel.add(passwordField);
 
-        GUIPanel.add(accountPanel);
+                GUIPanel.add(accountPanel);
+            }
+        }
     }
 }
-
