402c480e98ad9ef8179cfc1c212274dc9631451d
mfridsen <120990597+mfridsen@users.noreply.github.com>
2023-05-29 17:53:26 +0200

    Item unit test structure added, Entity class javadoxxed.

---
+++ src/test/java/model/entities/item/ItemCreationTest.java
@@ -0,0 +1,35 @@
+package model.entities.item;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+/**
+ * @author Mattias Fridsén
+ * @project LibraryDBMS
+ * @date 5/29/2023
+ * @contact matfir-1@student.ltu.se
+ * <p>
+ * Unit Test for the ItemCreation class.
+ * <p>
+ * Brought to you by copious amounts of nicotine.
+ */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ItemCreationTest
+{
+    /**
+     *
+     */
+    @Test
+    @Order(1)
+    void testItemCreation()
+    {
+        System.out.println("\n1: Testing ItemCreation...");
+        System.out.println("No test implemented here yet!");
+        //TODO Write your code here
+        System.out.println("\nTEST FINISHED.");
+    }
+}
