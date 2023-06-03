6db170a20ce34910747b5d96b2802151ec3de302
jespertruedsson <jeppe.truedsson@gmail.com>
2023-06-03 14:18:07 +0200

    14:17

---
+++ src/test/java/edu/groupeighteen/librarydbms/control/entities/author/AuthorHandlerTestSuite.java
@@ -0,0 +1,14 @@
+package edu.groupeighteen.librarydbms.control.entities.author;
+
+
+import edu.groupeighteen.librarydbms.control.BaseHandlerTest;
+
+/**
+ * @author Jesper Truedsson
+ * @project LibraryDBMS
+ * @date 2023-06-01
+ * Unit Test for the AuthorHandler class.
+ */
+public class AuthorHandlerTestSuite extends BaseHandlerTest {
+
+}
