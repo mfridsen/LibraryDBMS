5b572cf905d8b6a1c7a54c04fca786599f9c4095
jespertruedsson <jeppe.truedsson@gmail.com>
2023-06-03 14:34:31 +0200

    post-merge commit

--- src/test/java/edu/groupeighteen/librarydbms/control/entities/author/AuthorHandlerTestSuite.java
+++ src/test/java/edu/groupeighteen/librarydbms/control/entities/author/AuthorHandlerTestSuite.java
@@ -2,6 +2,8 @@ package edu.groupeighteen.librarydbms.control.entities.author;
 
 
 import edu.groupeighteen.librarydbms.control.BaseHandlerTest;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
 
 /**
  * @author Jesper Truedsson
@@ -9,6 +11,15 @@ import edu.groupeighteen.librarydbms.control.BaseHandlerTest;
  * @date 2023-06-01
  * Unit Test for the AuthorHandler class.
  */
+@Suite
+@SelectClasses({
+        CreateNewAuthorTest.class,
+        GetAuthorByIDTest.class,
+        UpdateAuthorTest.class,
+        DeleteAndUndoDeleteAuthorTest.class,
+        HardDeleteAuthorTest.class
+})
 public class AuthorHandlerTestSuite extends BaseHandlerTest {
+
 
 }
