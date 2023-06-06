24f0e3df54fd1f081a1ed9d10585fc48d048a228
mfridsen <120990597+mfridsen@users.noreply.github.com>
2023-06-06 14:07:28 +0200

    ReturnRental tested, 463/463

--- src/test/java/edu/groupeighteen/librarydbms/control/entities/rental/RentalHandlerTestSuite.java
+++ src/test/java/edu/groupeighteen/librarydbms/control/entities/rental/RentalHandlerTestSuite.java
@@ -18,7 +18,7 @@ import org.junit.platform.suite.api.Suite;
         GetOverdueRentalsTest.class,
         DeleteAndRecoverRentalTest.class,
         UpdateRentalTest.class,
-
+        ReturnRentalTest.class,
 
         //GetRentalsByRentalDayTest.class,
         //GetRentalsByRentalDateTest.class,
