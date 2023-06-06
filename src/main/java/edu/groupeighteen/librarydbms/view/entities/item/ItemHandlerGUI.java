672044d3dc52ed871c45940af2497160a49a2c8a
mfridsen <120990597+mfridsen@users.noreply.github.com>
2023-06-06 14:47:22 +0200

    second pre-merge commit

---
+++ src/main/java/edu/groupeighteen/librarydbms/view/entities/item/ItemHandlerGUI.java
@@ -0,0 +1,27 @@
+package edu.groupeighteen.librarydbms.view.entities.item;
+
+/**
+ * @author Mattias Fridsén
+ * @project LibraryDBMS
+ * @package edu.groupeighteen.librarydbms.view.entities.item
+ * @contact matfir-1@student.ltu.se
+ * @date 6/6/2023
+ * <p>
+ * We plan as much as we can (based on the knowledge available),
+ * When we can (based on the time and resources available),
+ * But not before.
+ * <p>
+ * Brought to you by enough nicotine to kill a large horse.
+ */
+public class ItemHandlerGUI
+{
+
+    public ItemHandlerGUI()
+    {
+
+    }
+
+    /*********************************** Getters and Setters are self-explanatory. ************************************/
+
+
+}
